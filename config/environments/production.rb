181b1294fc31c4ab5034cbba665756e0cfd088b3
Seth Dornbusch <sethdylan@gmail.com>
2016-07-26 15:04:00 +0800

    4.1 Added products

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   config.serve_static_files = true
-
+  config.public_file_server.enabled = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
