85a1d9ce3db22286e84fd92a2b688d7783580916
Seth Dornbusch <sethdylan@gmail.com>
2016-07-28 17:56:30 +0800

    added color and price to prodcuts attempted to add images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+def product_parms
+  params.require(:product).permit(:name, :description, :image_url, :color)
+end
