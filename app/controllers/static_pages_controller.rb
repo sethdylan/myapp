4c212829fa117c3d52ce20bfdc1770239570a537
Seth Dornbusch <sethdylan@gmail.com>
2016-08-01 20:04:16 +0800

    4.5 views, attempted to add carousel, still not seeing images on app page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
-  end
+end
 
- def landing_page
-    @featured_product = Product.first
-  end
+def landing_page
+  @products = Product.limit(3)
+end
 
 
 end
