65f637c14156228c825025b09bd6f4485cf4e95d
Seth Dornbusch <sethdylan@gmail.com>
2016-08-02 19:51:51 +0800

    4.5, was able to add images to goods page and carousel is also working with images

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/about'
 
-  get 'static_pages/products'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   root 'static_pages#landing_page'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  # get 'static_pages/about'
+  # get 'static_pages/products'
+
+  # get 'static_pages/contact'
+
+  # get 'static_pages/index'
+
+  # resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
