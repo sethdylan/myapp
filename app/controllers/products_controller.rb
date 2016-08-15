26bda0e311a470abe37d3d9d56a54db2c7ac9acc
Seth Dornbusch <sethdylan@gmail.com>
2016-08-15 18:10:44 +0800

    5.1 updated links on landing page to be dynamic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,8 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        redirect_to "/static_pages/landing_page"
+        # format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
