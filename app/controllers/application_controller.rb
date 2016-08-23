51b3bb665371bfc1f2af6edcb972987f5e4abdf2
Seth Dornbusch <sethdylan@gmail.com>
2016-08-23 15:46:44 +0800

    5.5 added authorizations on login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ end
 
 def product_parms
   params.require(:product).permit(:name, :description, :image_url, :color)
+
+
+rescue_from CanCan::AccessDenied do |exception|
+  redirect_to main_app.root_url, :alert => exception.message
+end
+
 end
