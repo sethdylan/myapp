14cf98efa423273a851688b5efcb31cb6c57b05f
Seth Dornbusch <sethdylan@gmail.com>
2016-08-26 15:42:35 +0800

    Edited controllers/application_controller.rb models/order.rb models/product.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
 
-def product_parms
-  params.require(:product).permit(:name, :description, :image_url, :color)
 
-
-rescue_from CanCan::AccessDenied do |exception|
+  rescue_from CanCan::AccessDenied do |exception|
   redirect_to main_app.root_url, :alert => exception.message
-end
+  end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+  has_many :orders
 end
 
