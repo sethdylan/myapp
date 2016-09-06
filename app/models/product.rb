6d5ac59486392e2e16e37d67d21949b246f016f6
Seth Dornbusch <sethdylan@gmail.com>
2016-09-07 04:43:03 +0800

    5.7 added comment model with scopes and custom methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 end
+
+def highest_rating_comment
+  comments.rating_desc.first
+end
+def lowest_rating_comment
+  comments.rating_asc.first
+end
+
+
 
