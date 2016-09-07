7783b6ae1fc9470c85e4c69f5d070a9f8906f0a9
Seth Dornbusch <sethdylan@gmail.com>
2016-09-06 18:03:45 -0600

    5.8 Comments Controller and Views

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,13 @@ end
 def highest_rating_comment
   comments.rating_desc.first
 end
+
 def lowest_rating_comment
   comments.rating_asc.first
+end
+
+def average_rating
+  comments.average(:rating).to_f
 end
 
 
