4de16125589a03f39babf8b3b8feb719686ded62
Seth Dornbusch <sethdylan@gmail.com>
2016-09-23 09:39:21 -0600

    5.8 fixed issue with rating/rate

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function(){
-  $('.rating').raty( { path: '/assets/images', scorename: 'comment[rating]'});
+  $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]'});
   $('.rated').raty({ path: '/assets/images',
     readOnly: true,
     score: function() {
