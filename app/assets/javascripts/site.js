f70e926268a6870eb153cbda61068a6d974d944f
Seth Dornbusch <sethdylan@gmail.com>
2016-10-13 17:25:11 -0600

    6.10 AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,15 @@
-$(document).on('turbolinks:load', function(){
-  $('.img-zoom').elevateZoom();
-  $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]'});
-  $('.rated').raty({ path: '/assets/images',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
-});
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets/images',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
 
+$(document).on('turbolinks:load ajaxSuccess', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
+});
