c79cc0399c149cfb4d963a047fd7a4bafe580ff8
Seth Dornbusch <sethdylan@gmail.com>
2016-10-25 10:13:43 -0600

    6.15 revision2

--- spec/controllers/static_pages_controller_test.rb
+++ spec/controllers/static_pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  context "should get index" do
     get static_pages_index_url
     assert_response :success
   end
